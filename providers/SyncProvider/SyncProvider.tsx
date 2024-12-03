7a8f2ec89f8dd7a3a5219c24ecd8a94b03b49464
kasparas-ban <kasparas.baniulis@gmail.com>
2024-12-03 09:44:35 +0200

    Fixed websocket address

--- providers/SyncProvider/SyncProvider.tsx
+++ providers/SyncProvider/SyncProvider.tsx
@@ -80,7 +80,10 @@ const initNewWs = (
   updateItem: (item: Partial<ItemResponse>) => void,
   deleteItem: (item_id: string) => void
 ) => {
-  const ws = new WebSocket(`ws://${BE_HOST}/sync`, [token, wsId])
+  const ws = new WebSocket(`${__DEV__ ? "ws" : "wss"}://${BE_HOST}/sync`, [
+    token,
+    wsId,
+  ])
 
   ws.onopen = () => {
     console.log("WebSocket connection established:", Platform.OS, wsId)
