be7ecf9525247a9bcaf90d44e020ea86d01b3c71
kasparas-ban <kasparas.baniulis@gmail.com>
2024-12-29 14:58:55 +0200

    Removed React Query persisters

--- providers/QueryProvider.tsx
+++ providers/QueryProvider.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from "react"
-import AsyncStorage from "@react-native-async-storage/async-storage"
 import NetInfo from "@react-native-community/netinfo"
-import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister"
-import { onlineManager, QueryClient } from "@tanstack/react-query"
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client"
+import {
+  onlineManager,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query"
 
 onlineManager.setEventListener(setOnline => {
   return NetInfo.addEventListener(state => {
@@ -19,23 +20,8 @@ export const queryClient = new QueryClient({
   },
 })
 
-const persister = createAsyncStoragePersister({
-  storage: AsyncStorage,
-  throttleTime: 3000,
-})
-
 export default function QueryProvider({ children }: { children: ReactNode }) {
-  const onCacheLoaded = async () => {
-    queryClient.invalidateQueries()
-  }
-
   return (
-    <PersistQueryClientProvider
-      onSuccess={onCacheLoaded}
-      client={queryClient}
-      persistOptions={{ persister }}
-    >
-      {children}
-    </PersistQueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
 }
